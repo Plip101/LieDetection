f20a07cb7d0e8a4526183899a54d43d8ff396494
Plip101 <Bazman15@gmail.com>
2017-11-30 17:28:22 +0000

    First Demo Iteration
    Working VR
    Color Changing
    Room built

---
+++ Assets/ColorChanging.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanging : MonoBehaviour {
+    int counter;
+    private GameObject wallcube, wallcube2, wallcube3;
+
+	// Use this for initialization
+	void Start () {
+        counter = 10;
+        wallcube = GameObject.Find("/Wall items/Cube");
+        wallcube2 = GameObject.Find("/Wall items/Cube (1)");
+        wallcube3 = GameObject.Find("/Wall items/Cube (2)");
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        counter += 1;  
+        if(Input.GetKeyDown("w"))
+        {
+            counter += 1;
+        }
+
+        if (counter > 10 && counter < 20 )
+        {
+            wallcube.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else { wallcube.GetComponent<Renderer>().material.color = Color.white; }
+
+        if (counter > 20 && counter < 30)
+        {
+            wallcube2.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else { wallcube2.GetComponent<Renderer>().material.color = Color.white; }
+
+        if (counter > 30 && counter < 40)
+        {
+            wallcube3.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else { wallcube3.GetComponent<Renderer>().material.color = Color.white; }
+
+   
+        if (counter >= 40)
+        {
+            counter = 0;
+        }
+        
+    }
+}
---
+++ Assets/QuestionIndicator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionIndicator : MonoBehaviour {
+    private GameObject table1, table2, table3, table4, table5, table6, table7, table8, table9, table10;
+    private GameObject[] tables_array;
+    int counter;
+    // Use this for initialization
+    void Start () {
+        
+        table1 = GameObject.Find("/Tabel items/Table1");
+        table2 = GameObject.Find("/Tabel items/Table2");
+        table3 = GameObject.Find("/Tabel items/Table3");
+        table4 = GameObject.Find("/Tabel items/Table4");
+        table5 = GameObject.Find("/Tabel items/Table5");
+        table6 = GameObject.Find("/Tabel items/Table6");
+        table7 = GameObject.Find("/Tabel items/Table7");
+        table8 = GameObject.Find("/Tabel items/Table8");
+        table9 = GameObject.Find("/Tabel items/Table9");
+        table10 = GameObject.Find("/Tabel items/Table10");
+        
+        counter = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject[] tables_array =
+            {
+            table1, table2, table3, table4, table5, table6,table7,table8,table9,table10
+            };
+        if (Input.GetKeyDown("space"))
+        {
+            tables_array[counter].GetComponent<Renderer>().material.color = Color.red;
+
+            print(counter);
+            counter += 1;
+
+            
+        }
+	}
+}
